f6af44488376ada0347c5a08b167583c5e121956
Димитрија Тимески <32930367+DimitrijaT@users.noreply.github.com>
2023-07-10 11:57:08 +0200

    Add creating pdf invoice from order and reading users from xlsx

--- CinemaTicketSystemAdminApp/Controllers/OrderController.cs
+++ CinemaTicketSystemAdminApp/Controllers/OrderController.cs
@@ -1,8 +1,10 @@
 ﻿using CinemaTicketSystemAdminApp.Models;
+using GemBox.Document;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net.Http;
 using System.Text;
@@ -12,6 +14,12 @@ namespace CinemaTicketSystemAdminApp.Controllers
 {
     public class OrderController : Controller
     {
+
+        public OrderController()
+        {
+            ComponentInfo.SetLicense("FREE-LIMITED-KEY");
+        }
+
         public IActionResult Index()
         {
             HttpClient client = new HttpClient(); // We can do get and post request using this
@@ -33,7 +41,7 @@ namespace CinemaTicketSystemAdminApp.Controllers
 
             HttpClient client = new HttpClient(); // We can do get and post request using this
 
-            string URL = "https://localhost:44343/api/Admin/GetDetailsForOrder";
+            string URI = "https://localhost:44343/api/Admin/GetDetailsForOrder";
 
             var model = new
             {
@@ -44,7 +52,7 @@ namespace CinemaTicketSystemAdminApp.Controllers
 
             HttpContent content = new StringContent(JsonConvert.SerializeObject(model), Encoding.UTF8, "application/json");
 
-            HttpResponseMessage responseMessage = client.PostAsync(URL, content).Result;
+            HttpResponseMessage responseMessage = client.PostAsync(URI, content).Result;
 
             // How do we get the data from the response?
 
@@ -54,7 +62,64 @@ namespace CinemaTicketSystemAdminApp.Controllers
         }
 
 
+        // We can change it to return FileContentResult but IActionResult and FileStreamResult also work
+
+        public FileContentResult CreateInvoice(Guid? orderId)
+        {
+            HttpClient client = new HttpClient(); // We can do get and post request using this
+
+            string URI = "https://localhost:44343/api/Admin/GetDetailsForOrder";
+
+            var model = new
+            {
+                Id = orderId
+            };
+
+            // Send the ID of the order we want to see to the Post request
+
+            HttpContent content = new StringContent(JsonConvert.SerializeObject(model), Encoding.UTF8, "application/json");
+
+            HttpResponseMessage responseMessage = client.PostAsync(URI, content).Result;
+
+            // How do we get the data from the response?
+
+            var data = responseMessage.Content.ReadAsAsync<Order>().Result;
+
+
+            var templatePath = Path.Combine(Directory.GetCurrentDirectory(), "Invoice.docx"); // Get the template docx document
+
+            var document = DocumentModel.Load(templatePath); // Use GemBox to load up this template
+
+            document.Content.Replace("{{OrderNumber}}", data.Id.ToString());
+            document.Content.Replace("{{UserName}}", data.User.UserName);
+
+            // This is for the List of Products
+            StringBuilder sb = new StringBuilder();
+
+            var totalPrice = 0;
+
+            foreach (var item in data.TicketOrders)
+            {
+                sb.AppendLine(item.OrderedTicket.MovieName + " with quantity of: " + item.Quantity + " and price of: $" + item.OrderedTicket.Price);
+                totalPrice += item.Quantity * item.OrderedTicket.Price;
+            }
+            document.Content.Replace("{{TicketList}}", sb.ToString());
+
+
+            document.Content.Replace("{{TotalPrice}}", "$" + totalPrice.ToString());
+
+
+            // Time to save them. But how? We are gonna transform them to a stream.
+
+            var stream = new MemoryStream();
+
+            document.Save(stream, new PdfSaveOptions());
+
+
+
+            return File(stream.ToArray(), new PdfSaveOptions().ContentType, "ExportInvoice.pdf");
+        }
+
+
     }
-
-
 }
---
+++ CinemaTicketSystemAdminApp/Controllers/UserController.cs
@@ -0,0 +1,103 @@
+﻿using CinemaTicketSystemAdminApp.Models;
+using ExcelDataReader;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CinemaTicketSystemAdminApp.Controllers
+{
+    public class UserController : Controller
+    {
+
+        [HttpGet("[action]")]
+        public IActionResult ImportUsers()
+        {
+            return View();
+        }
+
+
+        [HttpPost("[action]")]
+        // The name of this variable should be equal to the name="file" in the view
+        public IActionResult ImportUsers(IFormFile file)
+        {
+
+            // First we make a copy of the thing we are reading.
+            // NOTE: Make sure the entire path exists already, missing folders will NOT be created automatically
+            string pathToUpload = $"{Directory.GetCurrentDirectory()}\\Files\\{file.FileName}";
+
+            using (FileStream fileStream = System.IO.File.Create(pathToUpload))
+            {
+                file.CopyTo(fileStream);
+
+                fileStream.Flush();
+            }
+
+            // Second we read data from the copied file
+
+            List<User> users = getAllUsersFromFile(file.FileName);
+
+            // After we read the users we will make a Http client
+
+            HttpClient client = new HttpClient();
+
+            string URL = "https://localhost:44343/api/Admin/ImportAllUsers";
+
+            HttpContent content = new StringContent(JsonConvert.SerializeObject(users), Encoding.UTF8, "application/json");
+
+            string contentString = content.ReadAsStringAsync().Result;
+            Console.WriteLine(contentString);
+
+            HttpResponseMessage response = client.PostAsync(URL, content).Result;
+
+            var result = response.Content.ReadAsAsync<bool>().Result;
+
+            return RedirectToAction("ImportUsers", "Order");
+        }
+
+        private List<User> getAllUsersFromFile(string fileName)
+        {
+            string filePath = $"{Directory.GetCurrentDirectory()}\\Files\\{fileName}";
+
+            System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
+
+
+
+            List<User> users = new List<User>();
+
+            using (var stream = System.IO.File.Open(filePath, FileMode.Open, FileAccess.Read))
+            {
+
+                // Let's use the ExcelDataReader library:
+
+                using (var reader = ExcelReaderFactory.CreateReader(stream))
+                {
+                    while (reader.Read())
+                    {
+                        // We read row by row from the copy of the excel file
+
+                        users.Add(new Models.User
+                        {
+                            FirstName = reader.GetValue(0).ToString(),
+                            LastName = reader.GetValue(1).ToString(),
+                            Address = reader.GetValue(2).ToString(),
+                            Email = reader.GetValue(3).ToString(),
+                            Password = reader.GetValue(4).ToString(),
+                            ConfirmPassword = reader.GetValue(5).ToString(),
+                            PhoneNumber = reader.GetValue(6).ToString()
+                        });
+                    }
+                }
+            }
+            return users;
+
+        }
+
+    }
+}
